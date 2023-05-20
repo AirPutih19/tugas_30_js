433d778103aae2bf679d4d4992403240a987955a
AirPutih19 <kharismadwisj@gmail.com>
2023-05-20 15:37:48 +0700

    Tugas 30 JS
    Tugas 30 JS untuk niomic

---
+++ tugas30.js
@@ -0,0 +1,5 @@
+function panggilRegexp(value){
+    ambilData=value.match(/[ese]/g)
+    console.log(ambilData);
+}
+panggilRegexp("Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat");
